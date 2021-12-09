1862b2f57e03f580a52b4776512c33c2f8f88952
MM-Junaid <93264024+MM-Junaid@users.noreply.github.com>
2021-12-10 02:44:44 +0500

    Revert "shopify new"
    This reverts commit ddd68e64a3c1c6d2d53ac3574c53617ac7ca28fc.

--- ks_shopify/models/ks_shopify_sale_order.py
+++ ks_shopify/models/ks_shopify_sale_order.py
@@ -45,7 +45,7 @@ class KsSaleOrderInherit(models.Model):
                                          help="Checkout Id: Unique checkout ID of Shopify Sale Order")
     ks_sync_states = fields.Boolean(string="Sync Status",
                                     compute='compute_sync_status', readonly=True)
-    my_activity_date_deadline=fields.Date('my_activity_date_deadline')
+
 
     @api.onchange('ks_shopify_instance')
     def _ks_change_domain(self):
@@ -101,7 +101,8 @@ class KsSaleOrderInherit(models.Model):
             shopify_instance = self.env['ks.shopify.connector.instance'].search(
                 [('id', '=', vals.get('ks_shopify_instance'))])
             if shopify_instance and not shopify_instance.ks_default_order_prefix:
-                vals['name'] =str(vals.get('ks_order_name'))
+                shopify_prefix = shopify_instance.ks_custom_order_prefix.upper()
+                vals['name'] = shopify_prefix + ' #' + str(vals.get('ks_shopify_order_id'))
         return super(KsSaleOrderInherit, self).create(vals)
 
     def ks_cancel_sale_order_in_shopify(self):
